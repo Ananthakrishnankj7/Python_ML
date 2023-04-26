9e59cf83879c2800ebdb1ca1d9e1c9b691fdca8f
Ananthakrishnankj7 <ananthakrishnankj7@gmail.com>
2023-04-26 14:05:33 +0530

    Pattern

---
+++ Pattern.py
@@ -0,0 +1,66 @@
+# for i in range(1, 5):
+#     for j in range(1, 4):
+#         print(1, end=" ")
+#     print()
+
+
+
+# for i in range(1, 5):
+#     for j in range(1, 4):
+#         print('*', end=" ")
+#     print()
+
+# for i in range(1, 5):
+#     for j in range(1, 4):
+#         print(i, end=" ")
+#     print()
+
+# for i in range(1, 5):
+#     for j in range(1, 4):
+#         print(j, end=" ")
+#     print()
+
+
+# for i in range(1, 5):
+#     for j in range(1, 4):
+#         print(i+j, end=" ")
+#     print()
+
+
+# for i in range(1, 5):
+#     for j in range(1, 4):
+#         print(j+4, end=" ")
+#     print()
+
+
+# for i in range(1, 5):
+#     for j in range(i):
+#         print(i, end=" ")
+#     print()
+
+# n = int(input("Enter the No: "))
+# for i in range(1, n+1):
+#     for j in range(n+1-i):
+#         print(i, end=" ")
+#     print()
+
+
+# for i in range(1, 7):
+#     for j in range(1, 7):
+#         if i == 6 or j == 1 or i == j:
+#             print(" * ", end=" ")
+#         else:
+#             print("   ", end=" ")
+#     print()
+
+
+for i in range(1, 8):
+    for j in range(1, 8):
+        if j == 7 or j == 1 or i == 1 or i == 4:
+            print(" * ", end=" ")
+        else:
+            print("   ", end=" ")
+    print()
+
+
+
